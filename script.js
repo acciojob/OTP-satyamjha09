590ff57d8e2dd892049c9c3dbb2f297ba4350770
satyam jha <109567398+satyamjha09@users.noreply.github.com>
2025-03-05 15:31:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-//your JS code here. If required.
+ const inputs = document.querySelectorAll(".code");
+
+        function moveNext(input, index) {
+            if (input.value.length === 1 && index < inputs.length - 1) {
+                inputs[index + 1].focus();
+            }
+        }
+
+        function handleBackspace(event, index) {
+            if (event.key === "Backspace" && !inputs[index].value && index > 0) {
+                inputs[index - 1].focus();
+            }
+        }

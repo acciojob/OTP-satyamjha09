3ea9ee37cf6b5bc52e9f38fc26f210c08cf5f8c0
satyam jha <109567398+satyamjha09@users.noreply.github.com>
2025-03-05 15:33:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
- const inputs = document.querySelectorAll(".code");
+const inputs = document.querySelectorAll(".code");
 
-        function moveNext(input, index) {
-            if (input.value.length === 1 && index < inputs.length - 1) {
-                inputs[index + 1].focus();
-            }
+inputs.forEach((input, index) => {
+    input.addEventListener("input", (e) => {
+        if (e.inputType === "insertText" && input.value.length === 1 && index < inputs.length - 1) {
+            inputs[index + 1].focus();
         }
+    });
 
-        function handleBackspace(event, index) {
-            if (event.key === "Backspace" && !inputs[index].value && index > 0) {
-                inputs[index - 1].focus();
-            }
+    input.addEventListener("keydown", (e) => {
+        if (e.key === "Backspace" && !input.value && index > 0) {
+            inputs[index - 1].focus();
         }
+    });
+});
